acd123488db559d126a4b2c139b8c70f60f51cf2
sasakirione <sasakirione@gmail.com>
2022-09-23 18:08:19 +0900

    Re

--- src/main/kotlin/com/planet/lily/cddb/plugins/Routing.kt
+++ src/main/kotlin/com/planet/lily/cddb/plugins/Routing.kt
@@ -1,27 +1,33 @@
 package com.planet.lily.cddb.plugins
 
 import com.planet.lily.cddb.model.Album
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    val album = Album()
-
     routing {
         route("v1") {
-            route("albums") {
-                get {
-                    call.respond(album.getAlbumList())
-                }
-                route("{id}") {
-                    get {
-                        val id = call.parameters["id"]?.toInt() ?:
-                            return@get call.respondText("有効なアルバムIDを指定してください",
-                                status = io.ktor.http.HttpStatusCode.BadRequest)
-                        call.respond(album.getAlbum(id))
-                    }
-                }
+            album()
+        }
+    }
+}
+
+private fun Route.album() {
+    val album = Album()
+
+    route("albums") {
+        get {
+            call.respond(album.getAlbumList())
+        }
+        route("{id}") {
+            get {
+                val id = call.parameters["id"]?.toInt() ?: return@get call.respondText(
+                    "有効なアルバムIDを指定してください",
+                    status = HttpStatusCode.BadRequest
+                )
+                call.respond(album.getAlbum(id))
             }
         }
     }
