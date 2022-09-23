3417759749948d627fa96596e7a30a51f4a2b0f0
sasakirione <sasakirione@gmail.com>
2022-09-23 11:50:39 +0900

    Add: アルバム詳細取得機能を実装

--- src/main/kotlin/com/planet/lily/cddb/model/Album.kt
+++ src/main/kotlin/com/planet/lily/cddb/model/Album.kt
@@ -1,11 +1,16 @@
 package com.planet.lily.cddb.model
 
 import com.planet.lily.cddb.entity.Albums
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class Album {
     fun getAlbumList() = transaction {
         Albums.selectAll().map { it[Albums.id].value to (it[Albums.title] + " " + it[Albums.albumVersion].orEmpty()) }
+    }
+
+    fun getAlbum(id: Int) = transaction {
+        Albums.select { Albums.id eq id }.first()
     }
 }
--- src/main/kotlin/com/planet/lily/cddb/plugins/Routing.kt
+++ src/main/kotlin/com/planet/lily/cddb/plugins/Routing.kt
@@ -14,6 +14,12 @@ fun Application.configureRouting() {
                 get {
                     call.respond(album.getAlbumList())
                 }
+                route("{id}") {
+                    get {
+                        val id = call.parameters["id"]?.toInt() ?: return@get call.respondText("有効なアルバムIDを指定してください", status = io.ktor.http.HttpStatusCode.BadRequest)
+                        call.respond(album.getAlbum(id))
+                    }
+                }
             }
         }
     }
