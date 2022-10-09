2132373b2f2a9d75a2a0cbf35516b554ee9b8b58
sasakirione <sasakirione@gmail.com>
2022-10-09 11:24:57 +0900

    Fix: CORS設定の変更3

--- src/main/kotlin/com/planet/lily/cddb/plugins/HTTP.kt
+++ src/main/kotlin/com/planet/lily/cddb/plugins/HTTP.kt
@@ -6,13 +6,15 @@ import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
     install(CORS) {
-        allowMethod(HttpMethod.Get)
-        allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
-        anyHost()
+        allowHost("localhost")
+        allowHost("vercel.app")
+        allowHost("sasakirione.com")
         allowHeader("*")
+        allowCredentials = true
+        allowNonSimpleContentTypes = true
     }
 }
